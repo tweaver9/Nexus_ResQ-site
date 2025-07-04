14d5f45956f8da21dbde204952738e96ce16ce77
tweaver9 <tylerkweaver20@gmail.com>
2025-07-04 13:28:59 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -167,5 +167,4 @@ window.addEventListener('DOMContentLoaded', () => {
   loadRecentInspections();
 });
 
-<!-- FullCalendar JS -->
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js"></script>
