b3d047fb5ae76657b968ad78740586530c3426fc
tweaver9 <tylerkweaver20@gmail.com>
2025-07-25 21:59:31 -0500

    Delete In Progress directory

--- In Progress/react-dashboard/src/store/clientStore.js
+++
@@ -1,6 +0,0 @@
-// Zustand store for managing clientId context
-import { create } from 'zustand';
-
-// This file has been moved to src/store/clientStore.js
-// Please refer to the new location for the client store implementation.
-
